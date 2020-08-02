00cfc76ee8dc1f1af853c2c37d0cd443e96f8150
lalugue <51893731+lalugue@users.noreply.github.com>
2020-08-02 14:20:53 +0800

    update model with todo_deleted field

--- todo.model.js
+++ todo.model.js
@@ -2,19 +2,26 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let Todo = new Schema({
-    todo_description: {
-        type: String
+        todo_description: {
+            type: String
+        },
+        todo_responsible: {
+            type: String
+        },
+        todo_priority: {
+            type: String
+        },
+        todo_completed: {
+            type: Boolean
+        },
+        todo_deleted: {
+            type: Boolean
+        }    
     },
-    todo_responsible: {
-        type: String
-    },
-    todo_priority: {
-        type: String
-    },
-    todo_completed: {
-        type: Boolean
-    }
-})
+    { 
+        strict: false
+
+    })
 
 //create new Model named 'Todo' using schema Todo
 module.exports = mongoose.model('Todo',Todo)
